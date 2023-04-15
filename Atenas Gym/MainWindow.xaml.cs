9e967170bf4689a00d90148c33a9320b74478387
Andrés Rosales <Andrescod10@gmail.com>
2023-04-15 03:20:55 -0400

    Inicio

---
+++ Atenas Gym/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Atenas_Gym
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Cedula.Text == "Ingrese su cedula")
+            {
+                Cedula.Text = "";
+                Cedula.Foreground = Brushes.Black;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Cedula.Text == "")
+            {
+                Cedula.Text = "Ingrese su cedula";
+                Cedula.Foreground = Brushes.Gray;
+            }
+        }
+    }
+}
