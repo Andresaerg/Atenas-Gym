7a32d4af62c24249b6a423fbcbacde69998b5c12
Andres <andrescod10@gmail.com>
2023-05-03 17:01:32 -0400

    Progreso en la vista Recepción

---
+++ Atenas Gym/Model/ClientModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atenas_Gym.Model
+{
+    public class ClientModel
+    {
+        public string? Id { get; set; }
+        public string? Username { get; set; }
+        public string? Name { get; set; }
+        public string? Height { get; set; }
+        public string? Weight { get; set; }
+        public string? Waist { get; set; }
+        public string? Hips { get; set; }
+        public string? Arms { get; set; }
+        public string? Thights{ get; set; }
+    }
+}
