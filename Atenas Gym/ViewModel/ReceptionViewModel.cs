6f7e9a2531441aa2583bae1e880aeb1390cd2def
Andres <andrescod10@gmail.com>
2023-05-03 17:02:57 -0400

    Avance en vista de Recepción y Modelo de Clientes

--- Atenas Gym/ViewModel/ReceptionViewModel.cs
+++ Atenas Gym/ViewModel/ReceptionViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Atenas_Gym.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,13 @@ namespace Atenas_Gym.ViewModel
 {
     internal class ReceptionViewModel : ViewModelBase
     {
+        //Fields
+        private string? _clientID;
+        private string? _fullname;
+        private string? _Message;
+
+        private IUserRepository userRepository;
+
+        private ClientModel clientModel;
     }
 }
